e76776d937bce68bf0ca397bd9246cf963136871
Shawn Garbett <Shawn.P.Garbett@Vanderbilt.edu>
2017-10-13 15:57:08 -0500

    updated tests for Int and Slp

--- tests/testthat/testInt.R
+++ tests/testthat/testInt.R
@@ -2,7 +2,7 @@
 test_that("Acceptable Intercept Fit Absolute Error",
 {
   Fit.Int  <- acml.linear(y=odsInt$Y,
-                          x=as.matrix(cbind(1, odsInt[,c("time","snp","snptime","confounder")])),
+                          x=as.matrix(cbind(1, odsInt[,c("time","snp","confounder", "snptime")])),
                           z=as.matrix(cbind(1, odsInt$time)),
                           id=odsInt$id,
                           InitVals=c(5,  1, -2.5,  0.75,  0,  1.6094379,  0.2231436, -0.5108256,  1.6094379),
@@ -10,17 +10,22 @@ test_that("Acceptable Intercept Fit Absolute Error",
                           cutpoints=c(-2.569621, 9.992718),
                           SampProb=c(1, 0.1228, 1),
                           w.function="intercept")
-
-  Fit.Int2  <- acml.lmem(y=odsInt$Y,
-                          x=as.matrix(cbind(1, odsInt[,c("time","snp","snptime","confounder")])),
-                          z=as.matrix(cbind(1, odsInt$time)),
-                          id=odsInt$id,
+  
+  
+  odsInt$SampProbi <- 1
+  Fit.Int2  <- acml.lmem(Y~time*snp+confounder,
+                          ~time,
+                          data=odsInt,
+                          id=id,
                           InitVals=c(5,  1, -2.5,  0.75,  0,  1.6094379,  0.2231436, -0.5108256,  1.6094379),
                           ProfileCol=NA,
                           cutpoints=c(-2.569621, 9.992718),
                           SampProb=c(1, 0.1228, 1),
+                          SampProbiWL =SampProbi,
                           w.function="intercept")
 
-    expect_true( all(abs(Fit.Int$est - Fit.Int2$est) < 1e-10) )
+  expect_true( all(abs(Fit.Int$Ests - Fit.Int2$Ests) < 1e-10) )
+    
+  expect_true( all(diag(abs(Fit.Int$cov - Fit.Int2$cov)) < 1e-8) )
 })
   
--- tests/testthat/testSlp.R
+++ tests/testthat/testSlp.R
@@ -1,7 +1,7 @@
 test_that("Acceptable Slope Fit Absolute Error",
 {
   Fit.Slp  <- acml.linear(y=odsSlp$Y,
-                          x=as.matrix(cbind(1, odsSlp[,c("time","snp","snptime","confounder")])),
+                          x=as.matrix(cbind(1, odsSlp[,c("time","snp","confounder","snptime")])),
                           z=as.matrix(cbind(1, odsSlp$time)),
                           id=odsSlp$id,
                           InitVals=c(5,  1, -2.5,  0.75,  0,  1.6094379,  0.2231436, -0.5108256,  1.6094379),
@@ -9,17 +9,20 @@ test_that("Acceptable Slope Fit Absolute Error",
                           cutpoints=c(-0.7488912,  3.4557775),
                           SampProb=c(1, 0.1228, 1),
                           w.function="slope")
-
-  Fit.Slp2  <- acml.lmem(y=odsSlp$Y,
-                          x=as.matrix(cbind(1, odsSlp[,c("time","snp","snptime","confounder")])),
-                          z=as.matrix(cbind(1, odsSlp$time)),
-                          id=odsSlp$id,
+  odsSlp$SampProbi <- 1
+  Fit.Slp2  <- acml.lmem(Y~time*snp+confounder,
+                          ~time,
+                          data=odsSlp,
+                          id=id,
                           InitVals=c(5,  1, -2.5,  0.75,  0,  1.6094379,  0.2231436, -0.5108256,  1.6094379),
                           ProfileCol=NA,
                           cutpoints=c(-0.7488912,  3.4557775),
                           SampProb=c(1, 0.1228, 1),
+                          SampProbiWL =SampProbi,
                           w.function="slope")
 
-  expect_true( all(abs(Fit.Slp$est - Fit.Slp$est) < 1e-10) )
+  expect_true( all(abs(Fit.Slp$Ests - Fit.Slp2$Ests) < 1e-10) )
+    
+  expect_true( all(diag(abs(Fit.Slp$cov - Fit.Slp2$cov)) < 1e-8) )
 })
   
