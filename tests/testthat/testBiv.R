011bb9d906d048b4a5d16ed21c9f0359909f0d77
Shawn Garbett <Shawn.P.Garbett@Vanderbilt.edu>
2017-10-16 10:33:33 -0500

    Bivariate test updated for formula interface but failing

--- tests/testthat/testBiv.R
+++ tests/testthat/testBiv.R
@@ -2,7 +2,7 @@
 test_that("Acceptable Bivariate Fit Absolute Error",
 {
   Fit.Biv  <- acml.linear(y=odsBiv$Y,
-                          x=as.matrix(cbind(1, odsBiv[,c("time","snp","snptime","confounder")])),
+                          x=as.matrix(cbind(1, odsBiv[,c("time","snp","confounder","snptime")])),
                           z=as.matrix(cbind(1, odsBiv$time)),
                           id=odsBiv$id,
                           InitVals=c(5,  1, -2.5,  0.75,  0,  1.6094379,  0.2231436, -0.5108256,  1.6094379),
@@ -10,17 +10,21 @@ test_that("Acceptable Bivariate Fit Absolute Error",
                           cutpoints=c(-4.413225, 11.935188, -1.390172, 4.084768),
                           SampProb=c(0.122807, 1),
                           w.function="bivar")
-
-  Fit.Biv2 <- acml.lmem(y=odsBiv$Y,
-                        x=as.matrix(cbind(1, odsBiv[,c("time","snp","snptime","confounder")])),
-                        z=as.matrix(cbind(1, odsBiv$time)),
-                        id=odsBiv$id,
+  
+  odsBiv$SampProbi <- 1
+  Fit.Biv2 <- acml.lmem(Y~time*snp+confounder,
+                          ~time,
+                          data=odsBiv,
+                        id=id,
                         InitVals=c(5,  1, -2.5,  0.75,  0,  1.6094379,  0.2231436, -0.5108256,  1.6094379),
                         ProfileCol=NA,
                         cutpoints=c(-4.413225, 11.935188, -1.390172, 4.084768),
                         SampProb=c(0.122807, 1),
+                        SampProbiWL=SampProbi,
                         w.function="bivar")
 
-  expect_true( all(abs(Fit.Biv$est - Fit.Biv2$est) < 1e-10) )
+  expect_true( all(abs(Fit.Biv$Ests - Fit.Biv2$Ests) < 1e-10) )
+  
+  expect_true( all(diag(abs(Fit.Biv$cov - Fit.Biv2$cov)) < 1e-8) )
 })
   
